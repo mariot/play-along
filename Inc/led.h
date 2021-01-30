831910c1b8b1acca6fc969290efff3d9cfa6127d
Mariot Tsitoara <mariot.tsitoara@pm.me>
2021-01-30 12:49:21 +0100

    create led controlling library

---
+++ Inc/led.h
@@ -0,0 +1,12 @@
+//
+// Created by Mariot Tsitoara on 30/01/2021.
+//
+
+#ifndef PLAYALONG_LED_H
+#define PLAYALONG_LED_H
+
+void on_strum(int interval);
+void on_chord(char* chord);
+void on_wait(char* chord);
+
+#endif //PLAYALONG_LED_H
---
+++ Src/led.c
@@ -0,0 +1,20 @@
+//
+// Created by Mariot Tsitoara on 30/01/2021.
+//
+#include <stdio.h>
+#include <unistd.h>
+
+#include "led.h"
+
+void on_strum(int interval) {
+    printf("Strum\n");
+    sleep(interval / 1000);
+}
+
+void on_chord(char* chord) {
+    printf("Chord: %s\n", chord);
+}
+
+void on_wait(char* chord) {
+    printf("Wait for chord: %s\n", chord);
+}
